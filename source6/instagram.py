f3b3eaf5513579ded81a0e69420fe0080a5a3b55
psymonkey13 <psymonkey13@gmail.com>
2018-03-24 10:40:11 +0900

    강의자료

--- source6/instagram.py
+++ source6/instagram.py
@@ -3,24 +3,24 @@ from selenium.webdriver.common.keys import Keys
 import requests
 import time
 
-driver = webdriver.Chrome('./driver/chromedriver.exe')
-driver.get('https://www.instagram.com/accounts/login/')
-driver.find_element_by_xpath('//*[@name="username"]').send_keys('psymonkey13')
-driver.find_element_by_xpath('//*[@name="password"]').send_keys('mong&4176')
-driver.find_element_by_xpath('//*[@name="password"]').send_keys(Keys.ENTER)
+# driver = webdriver.Chrome('./driver/chromedriver.exe')
+# driver.get('https://www.instagram.com/accounts/login/')
+# driver.find_element_by_xpath('//*[@name="username"]').send_keys('psymonkey13')
+# driver.find_element_by_xpath('//*[@name="password"]').send_keys('mong&4176')
+# driver.find_element_by_xpath('//*[@name="password"]').send_keys(Keys.ENTER)
 
-time.sleep(3)
+# time.sleep(3)
 
-cookies= driver.get_cookies()
-s = requests.Session()
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.162 Safari/537.36'
-}
+# cookies= driver.get_cookies()
+# s = requests.Session()
+# headers = {
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.162 Safari/537.36'
+# }
 
-for cookie in cookies:
-    s.cookies.set(cookie['name'], cookie['value'], domain=cookie['domain'])
+# for cookie in cookies:
+#     s.cookies.set(cookie['name'], cookie['value'], domain=cookie['domain'])
 
-driver.quit()
+# driver.quit()
 
-print(s)
+# print(s)
 
